fb8eb4a368d3945eb1112b493955d860c83076c4
20230073 <20230073@students.htl-perg.ac.at>
2026-01-14 12:43:29 +0100

    Routing, Topbar, Pages

---
+++ Software/Frontend/WebApp/my-app/src/components/topbar.tsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+import Logo from '/src/pics/DS_Logo.png';
+import Einstellungen from '/src/pics/einstellungen.png';
+import '/src/styles/topbar.css';
+ 
+function Topbar() {
+    return (
+        <nav className='topbar'>
+            <Link to="/"><img src={Logo} alt="Logo" className="logo" width={200}/></Link>
+            <Link to="/trips" className='rides'>Fahrten</Link>
+            <Link to="/map">Karten</Link>
+            <Link to="/vehicles">Fahrzeuge</Link>
+            <Link to="/settings"><img src={Einstellungen} alt="Einstellungen" width={30} height={30}/></Link>
+            <button>Logout</button>
+        </nav>
+    );
+}
+
+export default Topbar;
---
+++ Software/Frontend/WebApp/my-app/src/pages/trips.tsx
@@ -0,0 +1,10 @@
+function Trips() {
+    return (
+        <div>
+            <h1>Trips</h1>
+            {/* Other components and content can be added here */}
+        </div>
+    );
+}
+
+export default Trips;
