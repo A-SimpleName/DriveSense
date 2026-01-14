9d17daadeaa8c2adee1bd1b974fd6d2c343fef51
20230073 <20230073@students.htl-perg.ac.at>
2026-01-14 12:43:29 +0100

    Routing, Topbar, Pages

---
+++ Software/Frontend/WebApp/my-app/src/pages/vehicles.tsx
@@ -0,0 +1,10 @@
+function Vehicles() {
+    return (
+        <div>
+            <h1>Vehicles</h1>
+            {/* Other components and content can be added here */}
+        </div>
+    );
+}
+
+export default Vehicles;
