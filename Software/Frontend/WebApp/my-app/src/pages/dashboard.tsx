3ae214e52f45d4de72f52e5e3890b2e9af4044fb
20230073 <20230073@students.htl-perg.ac.at>
2026-01-15 11:38:08 +0100

    kleine Aenderung

--- Software/Frontend/WebApp/my-app/src/pages/dashboard.tsx
+++ Software/Frontend/WebApp/my-app/src/pages/dashboard.tsx
@@ -2,19 +2,26 @@ import StatCard from "../components/statCard";
 import { Button } from "../components/button";
 import { Link } from "react-router-dom";
 
+export let rides = [
+    { id: 1,  date: "2024-01-15",km:120 },
+    { id: 2,  date: "2024-02-20", km:85 },
+    { id: 3,  date: "2024-03-10",km:95 },
+];
+
 function Dashboard() {
-    let rides = [
-        { id: 1,  date: "2024-01-15",km:120 },
-        { id: 2,  date: "2024-02-20", km:85 },
-        { id: 3,  date: "2024-03-10",km:95 },
-    ];
+    
     let lastRide = rides[rides.length - 1].km.toString() + " km | " + rides[rides.length - 1].date;
     let totalKm = rides.reduce((total, ride) => total + ride.km, 0).toString() + " km";
+    
     return (
         <div>
             <article>
-                <StatCard title="Letzte Fahrt" value= {lastRide}/>
-                <StatCard title="Gesamtstrecke: " value= {totalKm}/>
+                <div>
+                    <StatCard title="Letzte Fahrt" value= {lastRide}/>
+                </div>
+                <div>
+                    <StatCard title="Gesamtstrecke: " value= {totalKm}/>
+                </div>
             </article>
             <article>
                 <Link to="/trips"><Button label={"Fahrten ansehen"}/></Link>
