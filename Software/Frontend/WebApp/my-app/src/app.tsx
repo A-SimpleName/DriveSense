c5c7bfb338e7de8dd7459eae22a995ab4a1f1040
20230073 <20230073@students.htl-perg.ac.at>
2026-01-15 09:28:18 +0100

    Dashboard und Komponente

--- Software/Frontend/WebApp/my-app/src/app.tsx
+++ Software/Frontend/WebApp/my-app/src/app.tsx
@@ -3,10 +3,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DashboardPage from "./pages/dashboard";
 import TripsPage from "./pages/trips";
 import LoginPage from "./pages/login";
-import Topbar from "./components/topbar";
+import Topbar from "./components/Layout/topbar";
 import Vehicles from "./pages/vehicles";
 import Settings from "./pages/settings";
 import MapPage from "./pages/map";
+import Footer from "./components/Layout/footer";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         <Route path="/map" element={<MapPage />} />
         <Route path="*" element={<div>404 Not Found</div>} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
