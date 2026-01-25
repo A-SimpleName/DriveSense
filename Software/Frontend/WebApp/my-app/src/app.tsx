bdc62f07264da2f1f95c390706af81786d115e96
Christof Prevedel <20230094@students.htl-perg.ac.at>
2026-01-25 14:38:26 +0100

    Design changed

--- Software/Frontend/WebApp/my-app/src/app.tsx
+++ Software/Frontend/WebApp/my-app/src/app.tsx
@@ -7,22 +7,26 @@ import Vehicles from "./pages/vehicles";
 import Settings from "./pages/settings";
 import Footer from "./components/Layout/footer";
 import RideDetailPage from "./pages/rideDetailPage";
+import "./styles/app.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <Topbar />
-
-      <Routes>
-        <Route path="/" element={<DashboardPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/fahrten" element={<TripsPage />} />       
-        <Route path="/fahrzeuge" element={<Vehicles />} />
-        <Route path="/einstellungen" element={<Settings />} />
-        <Route path="/fahrten/:id" element={<RideDetailPage />} />
-        <Route path="*" element={<div>404 Not Found</div>} />
-      </Routes>
-      <Footer />
+      <div className="app-container">
+        <Topbar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<DashboardPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/fahrten" element={<TripsPage />} />
+            <Route path="/fahrzeuge" element={<Vehicles />} />
+            <Route path="/einstellungen" element={<Settings />} />
+            <Route path="/fahrten/:id" element={<RideDetailPage />} />
+            <Route path="*" element={<div>404 Not Found</div>} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
