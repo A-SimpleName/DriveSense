82df3751da6b949d95b3733a56c3c3cc894c126e
20230035 <20230035@students.htl-perg.ac.at>
2026-01-28 16:01:02 +0100

    (sehr frühen) prototyp für profile page hinzugefügt und orientation lock

--- Software/Frontend/App/drivesense/lib/pages/main_page.dart
+++ Software/Frontend/App/drivesense/lib/pages/main_page.dart
@@ -33,28 +33,22 @@ class _MainPageState extends State<MainPage> {
         onTap: (int index) {
           setState(() {
             _selectedIndexBottomNav = index;
-            //_changePageBody(index);
+            switch (index) {
+              case 0:
+                _currentAppBarTitle = 'Übersicht';
+                break;
+              case 1:
+                _currentAppBarTitle = 'Protokoll';
+                break;
+              case 2:
+                _currentAppBarTitle = 'Profil';
+                break;
+              default:
+                _currentAppBarTitle = 'Übersicht';
+            }
           });
         },
       ),
     );
   }
-
-
-  /* executed when switching to another tab in the bottom navigation */
-  /* void _changePageBody(int index) {
-    switch (index) {
-      case 0:
-        _currentAppBarTitle = 'Übersicht';
-        break;
-      case 1:
-        _currentAppBarTitle = 'Protokoll';
-        break;
-      case 2:
-        _currentAppBarTitle = 'Profil';
-        break;
-      default:
-        _currentAppBarTitle = 'Übersicht';
-    }
-  } */
 }
