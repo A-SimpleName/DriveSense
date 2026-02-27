9ed5b24b752b155f089e5f2ab4ca18c3067cc36d
20230035 <20230035@students.htl-perg.ac.at>
2026-02-27 17:10:18 +0100

    Fahrt tracken und in Tabelle einfügen (Runtimespeicher provisorisch)

--- Software/Frontend/App/drivesense/lib/model/trip.dart
+++ Software/Frontend/App/drivesense/lib/model/trip.dart
@@ -1,36 +1,37 @@
+import 'package:drivesense/model/trackingpoint.dart';
+
 class Trip {
   final int id;
   final int userId;
-  final int carId;
+  final int vehicleId;
   final DateTime startTime;
   final DateTime? endTime;
   final double distanceKm;
   final String weatherMain;
-  final String weatherDescription;
-  final String type;
+  final String? type;
+  final List<Trackingpoint>? trackingPoints;
 
   Trip({
     required this.id,
     required this.userId,
-    required this.carId,
+    required this.vehicleId,
     required this.startTime,
     this.endTime,
     required this.distanceKm,
     required this.weatherMain,
-    required this.weatherDescription,
     required this.type,
+    this.trackingPoints,
   });
 
   Map<String, dynamic> toJson() {
     return {
       "id": id,
       "userId": userId,
-      "carId": carId,
+      "vehicleId": vehicleId,
       "startTime": startTime.toIso8601String(),
       "endTime": endTime?.toIso8601String(),
       "distanceKm": distanceKm,
       "weatherMain": weatherMain,
-      "weatherDescription": weatherDescription,
       "type": type,
     };
   }
@@ -39,15 +40,28 @@ class Trip {
     return Trip(
       id: json["id"],
       userId: json["userId"],
-      carId: json["carId"],
+      vehicleId: json["vehicleId"],
       startTime: DateTime.parse(json["startTime"]),
       endTime: json["endTime"] != null
           ? DateTime.parse(json["endTime"])
           : null,
       distanceKm: json["distanceKm"].toDouble(),
       weatherMain: json["weatherMain"],
-      weatherDescription: json["weatherDescription"],
       type: json["type"],
+    );
+  }
+
+  Trip copyWith({required DateTime endTime, required double distanceKm, required List<Trackingpoint> trackingPoints}) {
+    return Trip(
+      id: id,
+      userId: userId,
+      vehicleId: vehicleId,
+      startTime: startTime,
+      endTime: endTime,
+      distanceKm: distanceKm,
+      weatherMain: weatherMain,
+      type: type,
+      trackingPoints: trackingPoints,
     );
   }
 }
---
+++ Software/Frontend/App/drivesense/lib/runtime_store.dart
@@ -0,0 +1,5 @@
+import 'package:drivesense/model/trip.dart';
+
+class RuntimeStore {
+  static final List<Trip> trips = [];
+}
