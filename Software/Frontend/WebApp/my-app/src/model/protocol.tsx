80182c6f9893c5a6aa2cc31fd7972758df1b25dd
20230017 <20230017@htl-perg.ac.at>
2026-02-27 14:21:09 +0100

    interface web

---
+++ Software/Frontend/WebApp/my-app/src/model/protocol.tsx
@@ -0,0 +1,5 @@
+interface protocol {
+    id: number;
+    tracking_id: number;
+    road_surface_conditions: string;
+}
---
+++ Software/Frontend/WebApp/my-app/src/model/tracking.tsx
@@ -0,0 +1,9 @@
+interface tracking  {
+    id: number
+    user_id: number;
+    car_id: number;
+    starttime: Date;
+    endtime: Date;
+    distance: number;
+    weather_main: string;
+}
