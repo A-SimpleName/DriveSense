35bc67690a504c2527e82f855c91caaae9020aec
Christof Prevedel <20230094@students.htl-perg.ac.at>
2026-03-05 10:45:06 +0100

    sync mit niklas website vehicles in table implementiert

--- Software/Frontend/WebApp/my-app/src/components/vehicles/table.tsx
+++ Software/Frontend/WebApp/my-app/src/components/vehicles/table.tsx
@@ -1,30 +1,45 @@
-import { vehicles } from "../../pages/vehicles";
-import { Button } from "../button";
+import { useEffect, useState } from "react"
+import { getAllVehicles } from "../../services/vehicleService"
+import type { Vehicle } from "../../model/vehicle"
+import "../../styles/table.css"
+import { Button } from "../button"
 
 function VehiclesTable() {
+    const [vehicles, setVehicles] = useState<Vehicle[]>([])
+
+    useEffect(() => {
+        getAllVehicles()
+            .then(data => setVehicles(data))
+            .catch(err => console.error("Fehler beim Laden:", err))
+    }, [])
+
     return (
         <table>
             <thead>
                 <tr>
                     <th>Model</th>
+                    <th>User</th>
                     <th>Kennzeichen</th>
                     <th>Kilometerstand</th>
-                    <th colSpan={2}>Aktionen</th>
+                    <th>Aktionen</th>
                 </tr>
             </thead>
             <tbody>
                 {vehicles.map(vehicle => (
-                    <tr key={vehicle.id}> 
-                        <td>{vehicle.name}</td>
-                        <td>{vehicle.licensePlate}</td>
-                        <td>{vehicle.kilometers}</td>
-                        <td><Button label="Bearbeiten"></Button></td>
-                        <td><Button label="Löschen"></Button></td>
+                    <tr key={vehicle.id}>
+                        <td>{vehicle.model}</td>
+                        <td>{vehicle.username}</td>
+                        <td>{vehicle.licenseplate}</td>
+                        <td>{vehicle.mileage} km</td>
+                        <td>
+                            <Button label="Bearbeiten" stopPropagation={true}/>
+                            <Button label="Löschen" stopPropagation={true}/>
+                        </td>
                     </tr>
                 ))}
             </tbody>
         </table>
-    );
+    )
 }
 
-export default VehiclesTable;
+export default VehiclesTable
--- Software/Frontend/WebApp/my-app/src/pages/vehicles.tsx
+++ Software/Frontend/WebApp/my-app/src/pages/vehicles.tsx
@@ -1,12 +1,6 @@
 import { Button } from "../components/button";
-// import VehiclesTable from "../components/vehicles/table";
-import {VehicleList} from "../components/vehicles/vehicle";
-
-export let vehicles = [
-    { id: 1, name: "Auto A",licensePlate: "AM08092" , kilometers: 97740 },
-    { id: 2, name: "Auto B",licensePlate: "PM12345" , kilometers: 45230 },
-    { id: 3, name: "Auto C",licensePlate: "LM67890" , kilometers: 12350 },
-];
+import VehiclesTable from "../components/vehicles/table";
+import { VehicleList } from "../components/vehicles/vehicle";
 
 function Vehicles() {
     
@@ -14,6 +8,8 @@ function Vehicles() {
         <div>
             <h1>Fahrzeuge</h1>
             <Button label={"+ Fahrzeuge hinzufügen"}/>
+            <VehiclesTable />
+      
             <VehicleList />
         </div>
     );
