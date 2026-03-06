46736302aeb1a337249edb64e20b796afa78e524
20230017 <20230017@htl-perg.ac.at>
2026-03-06 14:01:04 +0100

    vehicle-table.tesx abgeändert

--- Software/Frontend/WebApp/my-app/src/components/vehicles/table.tsx
+++ Software/Frontend/WebApp/my-app/src/components/vehicles/table.tsx
@@ -1,7 +1,18 @@
-import { vehicles } from "../../pages/vehicles";
+import { useEffect, useState } from "react";
+import { getAllVehicles } from "../../services/vehicleService";
+import type { Vehicle } from "../../model/vehicle";
 import { Button } from "../button";
 
 function VehiclesTable() {
+
+    const [vehicles, setVehicles] = useState<Vehicle[]>([])
+
+    useEffect(() => {
+        getAllVehicles()
+            .then(data => setVehicles(data))
+            .catch(err => console.error("Fehler beim Laden:", err))
+    }, [])
+
     return (
         <table>
             <thead>
@@ -12,17 +23,31 @@ function VehiclesTable() {
                     <th colSpan={2}>Aktionen</th>
                 </tr>
             </thead>
+
             <tbody>
+
                 {vehicles.map(vehicle => (
-                    <tr key={vehicle.id}> 
-                        <td>{vehicle.name}</td>
-                        <td>{vehicle.licensePlate}</td>
-                        <td>{vehicle.kilometers}</td>
-                        <td><Button label="Bearbeiten"></Button></td>
-                        <td><Button label="Löschen"></Button></td>
+
+                    <tr key={vehicle.id}>
+
+                        <td>{vehicle.model}</td>
+                        <td>{vehicle.licenseplate}</td>
+                        <td>{vehicle.mileage} km</td>
+
+                        <td>
+                            <Button label="Bearbeiten"/>
+                        </td>
+
+                        <td>
+                            <Button label="Löschen"/>
+                        </td>
+
                     </tr>
+
                 ))}
+
             </tbody>
+
         </table>
     );
 }
