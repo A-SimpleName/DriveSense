dbb82366a3a56a7c9caae34daace00fec015e28e
Christof Prevedel <20230094@students.htl-perg.ac.at>
2026-03-07 17:44:49 +0100

    neue Create-Statments für db

--- Software/Backend/src/main/java/com/drivesense/controller/TripController.java
+++ Software/Backend/src/main/java/com/drivesense/controller/TripController.java
@@ -1,5 +1,4 @@
 package com.drivesense.controller;
-import com.drivesense.db.TripDao;
 import com.drivesense.model.Trackingpoint;
 import com.drivesense.model.Trip;
 import com.drivesense.service.TripService;
