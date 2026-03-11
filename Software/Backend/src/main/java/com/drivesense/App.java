4838ae47adaad813eb2fca5dbeadf4474f62e3a7
20230073 <20230073@students.htl-perg.ac.at>
2026-03-11 21:15:14 +0100

    sync

--- Software/Backend/src/main/java/com/drivesense/App.java
+++ Software/Backend/src/main/java/com/drivesense/App.java
@@ -1,5 +1,6 @@
 package com.drivesense;
 
+import com.drivesense.service.GeocodingService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+       SpringApplication.run(App.class, args);
     }
 
 }
--- Software/Backend/src/main/java/com/drivesense/controller/ProfileController.java
+++ Software/Backend/src/main/java/com/drivesense/controller/ProfileController.java
@@ -21,7 +21,7 @@ public class ProfileController {
         return ResponseEntity.ok("Endpunkt profiles läuft");
     }
 
-    @GetMapping
+    @GetMapping("/")
     public ResponseEntity<List<Profile>> getAll() {
         return ResponseEntity.ok(profileService.getAll());
     }
@@ -38,7 +38,7 @@ public class ProfileController {
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping
+    @PostMapping("/")
     public ResponseEntity<Profile> insert(@RequestBody Profile profile) {
         return ResponseEntity.status(201).body(profileService.insert(profile));
     }
