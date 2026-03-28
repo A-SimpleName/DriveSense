72fb4e465e6623ff62438a38e092ea51a80cb873
20230035 <20230035@students.htl-perg.ac.at>
2026-03-28 13:17:15 +0100

    tripsummarydto und tripdao angepasst

--- Software/Backend/src/main/java/com/drivesense/dto/response/TripSummaryDto.java
+++ Software/Backend/src/main/java/com/drivesense/dto/response/TripSummaryDto.java
@@ -1,15 +1,80 @@
 package com.drivesense.dto.response;
 
+import java.time.LocalDateTime;
+
 public class TripSummaryDto {
     private int id;
+    private LocalDateTime startTime;
+    private int startMileage;
+    private int endMileage;
     private String accountFname;
     private String accountLname;
     private String vehicleModel;
-    private double distance;
+    private String licenseplate;
+    private String startPoint;
+    private String endPoint;
+    private int distance;
+    private String roadSurfaceConditions;
     private String type;
 
     public TripSummaryDto () {
 
+    }
+
+    public String getStartPoint() {
+        return startPoint;
+    }
+
+    public void setStartPoint(String startPoint) {
+        this.startPoint = startPoint;
+    }
+
+    public String getEndPoint() {
+        return endPoint;
+    }
+
+    public void setEndPoint(String endPoint) {
+        this.endPoint = endPoint;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public int getStartMileage() {
+        return startMileage;
+    }
+
+    public void setStartMileage(int startMileage) {
+        this.startMileage = startMileage;
+    }
+
+    public int getEndMileage() {
+        return endMileage;
+    }
+
+    public void setEndMileage(int endMileage) {
+        this.endMileage = endMileage;
+    }
+
+    public String getRoadSurfaceConditions() {
+        return roadSurfaceConditions;
+    }
+
+    public void setRoadSurfaceConditions(String roadSurfaceConditions) {
+        this.roadSurfaceConditions = roadSurfaceConditions;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 
     public int getId() {
@@ -44,19 +109,19 @@ public class TripSummaryDto {
         this.vehicleModel = vehicleModel;
     }
 
-    public double getDistance() {
+    public String getLicenseplate() {
+        return licenseplate;
+    }
+
+    public void setLicenseplate(String licenseplate) {
+        this.licenseplate = licenseplate;
+    }
+
+    public int getDistance() {
         return distance;
     }
 
-    public void setDistance(double distance) {
+    public void setDistance(int distance) {
         this.distance = distance;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
     }
 }
