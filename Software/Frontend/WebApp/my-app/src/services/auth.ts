83fb1fc6448741a3483d9566db115cc53cc65ba0
20230073 <20230073@students.htl-perg.ac.at>
2026-05-05 15:45:24 +0200

    group einlade service und emailservice

--- Software/Frontend/WebApp/my-app/src/services/auth.ts
+++ Software/Frontend/WebApp/my-app/src/services/auth.ts
@@ -1,63 +1,50 @@
-import { apiFetch } from "../api/api-token";
+// services/auth-service.ts
 
-const BASE_URL = "http://localhost:8080/api";
+import api from "../api/httpService";
 
-export async function signUp(fname: string, lname: string, email: string, password: string, birthdate: string) {
-  const res = await fetch(`${BASE_URL}/account/signUp`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ fname, lname, email, password, birthdate: birthdate.split("T")[0] })
+export async function signUp(
+  fname: string,
+  lname: string,
+  email: string,
+  password: string,
+  birthdate: string
+) {
+  return api.post("/account/signUp", {
+    fname,
+    lname,
+    email,
+    password,
+    birthdate: birthdate.split("T")[0],
   });
-
-  if (!res.ok) throw new Error("Registrierung fehlgeschlagen");
-  return await res.json();
 }
 
 export async function login(email: string, password: string) {
-  const res = await fetch("http://localhost:8080/api/account/login", {
-    method: "POST",
-    credentials: "include",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ email, password })
+  return api.post("/account/login", {
+    email,
+    password,
   });
-
-  if (!res.ok) throw new Error("Login fehlgeschlagen");
-
-  return res.json();
 }
 
 export async function logout() {
-  await fetch("http://localhost:8080/api/account/logout", {
-    method: "POST",
-    credentials: "include"
-  });
+  return api.post("/account/logout", {});
 }
 
 export async function logoutProfile() {
-    await fetch("http://localhost:8080/api/profiles/logout", {
-        method: "POST",
-        credentials: "include"
-    });
+  return api.post("/profiles/logout", {});
 }
 
 export async function checkAuth() {
-  const res = await apiFetch("http://localhost:8080/api/account");
-  return res.ok;
+  try {
+    await api.get("/account");
+    return true;
+  } catch {
+    return false;
+  }
 }
 
 export async function selectProfile(profileId: number) {
-  const res = await apiFetch(
-    `http://localhost:8080/api/account/select-profile?profileId=${profileId}`, {
-      method: "POST",
-      credentials: "include"
-    }
+  return api.post(
+    `/account/select-profile?profileId=${profileId}`,
+    {}
   );
-
-  if (!res.ok) throw new Error("Profil Auswahl fehlgeschlagen");
-
-  return res.json();
 }
