1013437d7e529920b8374623ba44d5660bbddd04
20230035 <20230035@students.htl-perg.ac.at>
2026-05-05 16:10:17 +0200

    updated allowed origins in webconfig.java

--- Software/Backend/src/main/java/com/drivesense/configs/WebConfig.java
+++ Software/Backend/src/main/java/com/drivesense/configs/WebConfig.java
@@ -15,8 +15,12 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://drivesense.htl-perg.ac.at"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
                         .allowCredentials(true);
             }
         };
