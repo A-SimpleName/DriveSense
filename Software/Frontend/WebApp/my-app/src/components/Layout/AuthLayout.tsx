3f2c7a4b73b57eaae831db7178e52c99a5a10377
Christof Prevedel <20230094@students.htl-perg.ac.at>
2026-05-05 22:37:13 +0200

    WebApp AuthContext fuer einfacheren Account/Benutzer zugriff, Design: User-Menu Dropwon, Select-Profile-Page, Buttons: Verbuggtes Design

--- Software/Frontend/WebApp/my-app/src/components/Layout/AuthLayout.tsx
+++ Software/Frontend/WebApp/my-app/src/components/Layout/AuthLayout.tsx
@@ -3,17 +3,12 @@ import Topbar from "./topbar";
 
 type AuthLayoutProps = {
     children: React.ReactNode;
-    setAccount: React.Dispatch<React.SetStateAction<any>>;
-    account: any;
-    onProfileSelect: () => void;
-    setIsAuth: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-export function AuthLayout({ children, setAccount, account, onProfileSelect, setIsAuth }: AuthLayoutProps) {
-
+export function AuthLayout({ children }: AuthLayoutProps) {
     return ( 
         <div className="app-container">
-            <Topbar setAccount={setAccount} account={account} onProfileSelect={onProfileSelect} setIsAuth={setIsAuth} />
+            <Topbar />
             <main className="main-content">
                 {children}
             </main>
